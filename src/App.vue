eb3c26aac9ed35d71e7868cb5be4835ff5193f7e
pandzic.ivan <pandzic.ivan@nsoft.com>
2019-06-12 09:36:30 +0200

    Add GatewayExample component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" src="./assets/logo.png">
+    <GatewayExample />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GatewayExample from './components/GatewayExample.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GatewayExample
   }
 }
 </script>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  max-height: 200px;
 }
 </style>
---
+++ src/components/GatewayExample.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to Your Seven Intergation Example</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the project README.
+    </p>
+  </div>
+</template>
+
+<script>
+const SlaveGateway = window.gravity.gateway.slave;
+const gateway = new SlaveGateway({
+  slaveId : 'GatewayPlayground',
+  data: {
+    foo: 'bar',
+  },
+  debug: true,
+});
+gateway.subscribe('*', (message) => { // eslint-disable-line
+  // Gateway message received.
+  // TODO: Define method
+});
+export default {
+  name: 'GatewayExample',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
