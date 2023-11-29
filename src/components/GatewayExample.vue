7665f3fc7af4d0eeb172ef89bcc0e32ab3c3fd29
Dinko <miletic.dinko@nsoft.com>
2023-11-29 09:00:55 +0100

    Improve

--- src/components/GatewayExample.vue
+++ src/components/GatewayExample.vue
@@ -1,9 +1,14 @@
+<!-- eslint-disable no-console -->
 <template>
   <div class="hello">
     <div class="actions">
       <button @click="removeIframe">Remove iframe</button>
+      <button @click="addIframe">Add iframe</button>
+      <h4>Iframe url</h4>
+      <input v-model="url" type="text">
       <h4>Iframe:</h4>
-      <iframe id="iframee" src="https://memory-leak.surge.sh" frameborder="0"></iframe>
+      <div id="append">
+      </div>
     </div>
   </div>
 </template>
@@ -14,23 +19,31 @@ export default {
   name: 'GatewayExample',
   data() {
     return {
-      eventName: '',
-      eventData: '',
-      jsonEditorOptions: {
-        mode: 'text',
-        modes: ['text', 'code'],
-      },
+      elementToCreate: 'iframe',
+      url: 'http://localhost:8080',
     }
   },
   mounted() {
-
+    window.addEventListener('message', (e, data) => {
+      // eslint-disable-next-line no-console
+      console.log('messae recievied', data);
+    });
+     // eslint-disable-next-line no-console
+    console.log('window parent exist', window.parent);
   },
   methods: {
+    addIframe() {
+      let iframe = window.document.createElement(this.elementToCreate);
+      iframe.setAttribute('src', this.url);
+      iframe.setAttribute('style', 'width:640px; height:480px');
+      iframe.setAttribute('nwdisable', '');
+      let append = document.querySelector('#append');
+      append.append(iframe);
+    },
     removeIframe() {
-      let i = document.querySelector('iframe');
+      let i = document.querySelector(this.elementToCreate);
       i.parentNode.removeChild(i);
       i = null;
-
     },
   },
 }
@@ -38,6 +51,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+iframe, webview {
+  border: 2px solid red;
+}
 
 h3 {
   margin: 40px 0 0;
